63f9eaefbb47fd7f081fcd7bd602383c790ff22f
Daniel Steinhauer <d.steinhauer@uni-bremen.de>
2022-05-02 15:31:53 +0200

    Initial commit.

---
+++ src/main/java/de/uni_bremen/see/arborext/App.java
@@ -0,0 +1,13 @@
+package de.uni_bremen.see.arborext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
